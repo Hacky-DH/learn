b6c32d328181cac17ff7c6816aee6256bd00334a
Hacky-DH <dhgacky@gmail.com>
2019-03-31 18:51:52 +0800

    two way of async

--- net/tornado/app.py
+++ net/tornado/app.py
@@ -3,9 +3,11 @@
 from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
 
+
 class MainHandler(RequestHandler):
     def get(self):
         self.write("Hello, world")
+
 
 if __name__ == "__main__":
     app = Application([(r"/", MainHandler),])
--- net/tornado/asyncclient.py
+++ net/tornado/asyncclient.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python
 
 from tornado.httpclient import AsyncHTTPClient
+from tornado.ioloop import IOLoop
 from tornado import gen
-import asyncio
+import sys
 
-@gen.coroutine
-def fetch(url):
-    http_client = AsyncHTTPClient()
-    response = yield http_client.fetch(url)
-    # Python < 3.3
-    # raise gen.Return(response.body)
-    # Python >= 3.3
-    return response.body
+PY33 = sys.version_info[:2] >= (3, 3)
+PY35 = sys.version_info[:2] >= (3, 5)
 
-async def main():
-	r = await fetch("http://www.example.com")
-	print(r)
+URL = "http://www.example.com"
+
+if PY35:
+	import asyncio
+	async def fetch(url):
+		"""use async and await"""
+		http_client = AsyncHTTPClient()
+		response = await http_client.fetch(url)
+		return response.body
+
+	def main():
+		content = asyncio.run(fetch(URL))
+		print(content[:20])
+else:
+	@gen.coroutine
+	def fetch(url):
+		"""
+		use yield generator
+
+		:return Future
+		"""
+		http_client = AsyncHTTPClient()
+		response = yield http_client.fetch(url)
+		if PY33:
+			return response.body
+		else:
+			raise gen.Return(response.body)
+
+	def main():
+		content = IOLoop().run_sync(lambda: fetch(URL))
+		print(content[:20])
 
 if __name__ == "__main__":
-	asyncio.run(main())
+	main()
