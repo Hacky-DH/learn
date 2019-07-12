7c3e501e1c73b06f55ab1b106957d84392b3d9f2
Hacky-DH <dhgacky@gmail.com>
2019-07-13 00:07:20 +0800

    deserialize 1

--- cpplib/rapidjson/serialize.cpp
+++ cpplib/rapidjson/serialize.cpp
@@ -1,9 +1,13 @@
 // rapidjson doc http://rapidjson.org/zh-cn/index.html
+// demo of serialize/deserialize cpp object
+// build g++ -std=c++1z serialize.cpp
 #include <iostream>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <fstream>
+#include <sstream>
+#include <string>
 #include <rapidjson/document.h>
 #include <rapidjson/pointer.h>
 #include <rapidjson/writer.h>
@@ -15,6 +19,7 @@ using std::vector;
 using std::map;
 using std::string;
 using std::ifstream;
+using std::stringstream;
 using std::cerr;
 using std::cout;
 using std::endl;
@@ -27,39 +32,91 @@ using rapidjson::GetParseError_En;
 // json demo from https://github.com/alibaba/euler/wiki/Preparing-Data
 
 class Edge {
-	long src_id;
-	long dst_id;
+	int64_t src_id;
+	int64_t dst_id;
 	int edge_type;
-	float weight;
-	map<int, vector<long>> uint64_feature;
-	map<int, vector<float>> float_feature;
+	double weight;
+	map<int, vector<int64_t>> uint64_feature;
+	map<int, vector<double>> float_feature;
 	map<int, string> binary_feature;
-protected:
+public:
 	template <typename Writer>
-    void serialize(Writer& writer) const {
-    }
-    void deserialize(Document& doc) {
-    }
+	void serialize(Writer& writer) const {
+	}
+
+	void deserialize(Document& doc) {
+	}
 };
 
 
 class Block {
 private:
-	long node_id;
+	int64_t node_id;
 	int node_type;
-	float node_weight;
-	map<int, map<long, float>> neighbor;
-	map<int, vector<long>> uint64_feature;
-	map<int, vector<float>> float_feature;
+	double node_weight;
+	map<int, map<int64_t, double>> neighbor;
+	map<int, vector<int64_t>> uint64_feature;
+	map<int, vector<double>> float_feature;
 	map<int, string> binary_feature;
 	vector<Edge> edge;
-protected:
+public:
 	template <typename Writer>
-    void serialize(Writer& writer) const {
-    }
-    void deserialize(Document& doc) {
-    }
+	void serialize(Writer& writer) const {
+	}
+
+	void deserialize(Document& doc) {
+		Value* v = Pointer("/node_id").Get(doc);
+		if(v) node_id = v->GetInt64();
+		v = Pointer("/node_type").Get(doc);
+		if(v) node_type = v->GetInt();
+		v = Pointer("/node_weight").Get(doc);
+		if(v) node_weight = v->GetDouble();
+		v = Pointer("/neighbor").Get(doc);
+		if(v) node_weight = v->GetDouble();
+	}
+
+	string str() const {
+		stringstream ss;
+		ss<<"Block: "<<endl;
+		ss<<"node_id: "<<node_id<<endl;
+		ss<<"node_type: "<<node_type<<endl;
+		ss<<"node_weight: "<<node_weight<<endl;
+		ss<<"neighbor: ";
+		for (auto const& [type, nb] : neighbor) {
+			ss<<type;
+			for (auto const& [id, weight] : nb) {
+				ss<<id<<weight<<" ";
+			}
+			ss<<";";
+		}
+		ss<<endl;
+		ss<<"uint64_feature: ";
+		for (auto const& [id, val] : uint64_feature) {
+			ss<<id;
+			for (auto const& v : val) {
+				ss<<v<<" ";
+			}
+			ss<<";";
+		}
+		ss<<endl;
+		ss<<"float_feature: ";
+		for (auto const& [id, val] : float_feature) {
+			ss<<id;
+			for (auto const& v : val) {
+				ss<<v<<" ";
+			}
+			ss<<";";
+		}
+		ss<<endl;
+		ss<<"binary_feature: ";
+		for (auto const& [id, val] : binary_feature) {
+			ss<<id<<val<<" ";
+		}
+		ss<<endl;
+		return ss.str();
+	}
 };
+
 
 int test(const string& file_name) {
 	ifstream stream(file_name);
@@ -69,9 +126,12 @@ int test(const string& file_name) {
 		cerr<<"parse error: "<<GetParseError_En(doc.GetParseError())<<endl;
 		return 1;
 	}
-    assert(doc.IsObject());
-    stream.close();
-    return 0;
+	assert(doc.IsObject());
+	Block block;
+	block.deserialize(doc);
+	cout<<block.str()<<endl;
+	stream.close();
+	return 0;
 }
 
 
@@ -87,5 +147,3 @@ void lambda() {
 int main() {
 	return serialize::test("data.json");
 }
-
-//serialize/deserialize
