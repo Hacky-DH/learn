8937b8147a85b75da898673767f4a9b26628d200
Hacky-DH <dhgacky@gmail.com>
2019-07-25 23:49:58 +0800

    cpp tempate

---
+++ cpplib/effective/template.cpp
@@ -0,0 +1,23 @@
+template<typename T>
+class Rational {
+	T num, den;
+public:
+	Rational(const T& n=0, const T& d=1):num(n),den(d) {}
+	const T numerator() const {return num;}
+	const T denominator() const {return den;}
+	friend const Rational operator*(const Rational& l, const Rational& r){
+		return deMultiply(l ,r);
+	}
+};
+
+template<typename T>
+const Rational<T> deMultiply(const Rational<T>& l, const Rational<T>& r) {
+	return Rational<T>(l.numerator()*r.numerator(), l.denominator()*r.denominator());
+}
+
+int main()
+{
+	Rational<int> a(1, 2);
+	Rational<int> res = a * 2;
+	return 0;
+}
