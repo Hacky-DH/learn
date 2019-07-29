466fe51a39372955a1396ce160bb69971743df1b
Hacky-DH <dhgacky@gmail.com>
2019-07-29 23:45:18 +0800

    placement new/delete

--- cpplib/effective/newhandler.cpp
+++ cpplib/effective/newhandler.cpp
@@ -4,6 +4,11 @@
 
 using std::new_handler;
 
+// all global operator new:
+// void* operator new(std::size_t);
+// void* operator new(std::size_t, void*) noexcept;
+// void* operator new(std::size_t, const std::nothrow_t&) noexcept;
+
 // new_hander RAII
 class NewhandlerHolder {
 	new_handler handler;
@@ -24,6 +29,7 @@ class Newhandler {
 public:
 	static new_handler set_new_handler(new_handler) noexcept;
 	static void* operator new(std::size_t);
+	static void operator delete(void*) noexcept;
 };
 
 template<typename T>
@@ -43,19 +49,44 @@ void* Newhandler<T>::operator new(std::size_t size) {
 	return ::operator new(size);
 }
 
-void outOfMem(){
+template<typename T>
+void Newhandler<T>::operator delete(void* ptr) noexcept {
+	std::cout<<"call class-owned operator delete"<<std::endl;
+	return ::operator delete(ptr);
+}
+
+void outOfMem() {
 	std::cerr<<"out of memory"<<std::endl;
 	std::abort();
 }
 
 // test class
 class Widget: public Newhandler<Widget> {
-
+public:
+	// let super new and delete visible
+	using Newhandler<Widget>::operator new;
+	using Newhandler<Widget>::operator delete;
+	// placement new and delete
+	static void* operator new(std::size_t, std::ostream&);
+	// call this when exception in constructor
+	static void operator delete(void*, std::ostream&) noexcept;
 };
+
+void* Widget::operator new(std::size_t size, std::ostream& out) {
+	out<<"call class-owned placement operator new"<<std::endl;
+	return ::operator new(size);
+}
+
+void Widget::operator delete(void* ptr, std::ostream& out) noexcept {
+	out<<"call class-owned placement operator delete"<<std::endl;
+	return ::operator delete(ptr);
+}
 
 int main()
 {
 	Widget::set_new_handler(outOfMem);
 	std::shared_ptr<Widget> pw(new Widget);
+	Widget *ppw = new (std::cerr) Widget;
+	delete ppw;
 	return 0;
 }
