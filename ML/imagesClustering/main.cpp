3a91658f7d4185cedbd46a587c1381dc2262d75f
Hacky-DH <dhgacky@gmail.com>
2019-11-13 09:21:52 +0800

    transform dataset and create data loader

--- ML/imagesClustering/main.cpp
+++ ML/imagesClustering/main.cpp
@@ -73,22 +73,27 @@ int main(int argc, char* argv[]) {
         //train-images-idx3-ubyte train-labels-idx1-ubyte
         //t10k-images-idx3-ubyte t10k-labels-idx1-ubyte
         namespace td = torch::data::datasets;
-        auto train_data = td::MNIST(data_root.string());
-        auto test_data = td::MNIST(data_root.string(), td::MNIST::Mode::kTest);
-        cout << "load mnist train size " << train_data.size().value()
-            << ", test size " << test_data.size().value() << endl;
+        auto _train_data = td::MNIST(data_root.string());
+        auto _test_data = td::MNIST(data_root.string(), td::MNIST::Mode::kTest);
+        cout << "load mnist train size " << _train_data.size().value()
+            << ", test size " << _test_data.size().value() << endl;
+
+        namespace tt = torch::data::transforms;
+        auto train_data = _train_data.map(tt::Normalize<>(0.13707, 0.3081)).
+            map(torch::data::transforms::Stack<>());
+        auto data_loader = torch::data::make_data_loader(std::move(train_data), 64);
+
+        auto net = std::make_shared<Net>();
+        torch::optim::SGD optimizer(net->parameters(), 0.01);
+
+        for (auto& batch : *data_loader) {
+            cout << batch.data.size(0) << endl;
+        }
     }
     catch (const std::exception& e) {
         cerr << e.what() << endl;
+        return 1;
     }
-
-    //auto dataset = torch::data::datasets::MNIST(data_root.string()).
-    //    map(torch::data::transforms::Normalize<>(0.13707, 0.3081)).
-    //    map(torch::data::transforms::Stack<>());
-    //auto data_loader = torch::data::make_data_loader(std::move(dataset), 64);
-
-    auto net = std::make_shared<Net>();
-    torch::optim::SGD optimizer(net->parameters(), 0.01);
 }
 
 // class Options
