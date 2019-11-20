3501b0bf1e56833d8d7de885d065c130c68bcc2c
Hacky-DH <dhgacky@gmail.com>
2019-11-20 09:26:19 +0800

    test Accuracy for mnist

--- ML/imagesClustering/include/train.h
+++ ML/imagesClustering/include/train.h
@@ -5,34 +5,31 @@ namespace nn = torch::nn;
 
 template <typename Module, typename DataLoader>
 void train(
+    size_t epoch,
     Module model,
     torch::optim::Optimizer &optimizer,
     DataLoader &data_loader,
-    size_t start_epoch = 0,
-    size_t num_epochs = 10,
-    size_t log_per_steps = 50,
+    size_t log_per_steps = 100,
     size_t checkpoint_per_epoch = 5,
     std::string model_dir = "model") {
     model->train();
-    for (size_t epoch = start_epoch; epoch < num_epochs; ++epoch) {
-        size_t step = 0;
-        for (auto& batch : *data_loader) {
-            optimizer.zero_grad();
-            // Execute the model on the input data.
-            torch::Tensor prediction = model->forward(batch.data);
-            torch::Tensor loss = torch::nll_loss(prediction, batch.target);
-            loss.backward();
-            optimizer.step();
-            if (++step % log_per_steps == 0) {
-                std::cout << "Epoch: [" << epoch + 1 << "] Batch: [" << step
-                    << "] Loss: " << loss.item<float>() << std::endl;
-            }
+    size_t step = 0;
+    for (auto& batch : *data_loader) {
+        optimizer.zero_grad();
+        // Execute the model on the input data.
+        torch::Tensor prediction = model->forward(batch.data);
+        torch::Tensor loss = torch::nll_loss(prediction, batch.target);
+        loss.backward();
+        optimizer.step();
+        if (++step % log_per_steps == 0) {
+            std::cout << "Epoch: [" << epoch + 1 << "] Batch: [" << step
+                << "] Loss: " << loss.item<float>() << std::endl;
         }
-        if ((epoch + 1) % checkpoint_per_epoch == 0) {
-            fs::create_directory(model_dir);
-            fs::path file = fs::path(model_dir) / "checkpoint.pt";
-            torch::save(model, file.string());
-        }
+    }
+    if ((epoch + 1) % checkpoint_per_epoch == 0) {
+        fs::create_directory(model_dir);
+        fs::path file = fs::path(model_dir) / "checkpoint.pt";
+        torch::save(model, file.string());
     }
 }
 
@@ -46,16 +43,15 @@ void test(
     double test_loss = 0;
     int32_t correct = 0;
     for (const auto& batch : *data_loader) {
-        auto output = model.forward(batch.data);
+        auto output = model->forward(batch.data);
+        // no at::Reduction::Sum?
         test_loss += torch::nll_loss(output, batch.target,
-            /*weight=*/{}, at::Reduction::Sum)
+            /*weight=*/{}, 2)
             .template item<float>();
         auto pred = output.argmax(1);
         correct += pred.eq(batch.target).sum().template item<int64_t>();
     }
     test_loss /= dataset_size;
-    std::printf(
-        "\nTest set: Average loss: %.4f | Accuracy: %.3f\n",
-        test_loss,
-        static_cast<double>(correct) / dataset_size);
+    std::cout << "\nTest set: Average loss: " << test_loss << " | Accuracy: " <<
+        static_cast<double>(correct) / dataset_size << std::endl;
 }
--- ML/imagesClustering/src/main.cpp
+++ ML/imagesClustering/src/main.cpp
@@ -70,16 +70,28 @@ int main(int argc, char* argv[]) {
         cout << "load mnist train size " << mnist[0].size().value()
             << ", test size " << mnist[1].size().value() << endl;
 
-        auto train_data = mnist[0].map(tt::Normalize<>(0.13707, 0.3081)).
-            map(tt::Stack<>());
+        auto train_data = mnist[0].map(tt::Normalize<>(0.13707, 0.3081))
+            .map(tt::Stack<>());
         int batch_size = options.batch_size();
-        auto data_loader = torch::data::make_data_loader(std::move(train_data), batch_size);
+        auto train_data_loader = torch::data::make_data_loader(
+            std::move(train_data), batch_size);
+
+        auto test_data = mnist[1].map(tt::Normalize<>(0.1307, 0.3081))
+            .map(tt::Stack<>());
+        const size_t test_data_size = test_data.size().value();
+        auto test_data_loader = torch::data::make_data_loader(
+                std::move(test_data), 1024);
 
         auto model = std::make_shared<Net>();
         torch::optim::SGD optimizer(model->parameters(),
             torch::optim::SGDOptions(0.01).momentum(0.5));
-        train(model, optimizer, data_loader);
 
+        // train and test
+        size_t start_epoch = 0, num_epochs = 10;
+        for (size_t epoch = start_epoch; epoch < num_epochs; ++epoch) {
+            train(epoch, model, optimizer, train_data_loader);
+            test(model, test_data_loader, test_data_size);
+        }
     } catch (const std::exception& e) {
         cerr << e.what() << endl;
         return 1;
