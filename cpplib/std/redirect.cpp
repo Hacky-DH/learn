cac4234c32a4f2e5c4f0163ac4ef2b64f7547e18
Hacky-DH <dhgacky@gmail.com>
2019-12-24 18:06:43 +0800

    redirect

---
+++ cpplib/std/redirect.cpp
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+void redirect1() {
+    std::cout<<"redirect using dup"<<std::endl;
+    auto oldout = dup(STDOUT_FILENO);
+    auto devnull = open("/dev/null", O_WRONLY);
+    dup2(devnull, STDOUT_FILENO);
+    std::cout<<"stdout to dev null"<<std::endl;
+    std::cerr<<"stderr is ok"<<std::endl;
+    dup2(oldout, STDOUT_FILENO);
+    std::cout<<"stdout is ok again"<<std::endl;
+}
+
+//  use freopen
+void redirect2() {
+    std::cout<<"redirect using freopen"<<std::endl;
+    std::cout<<"stdout is ok"<<std::endl;
+    freopen("/dev/null", "w", stdout);
+    std::cout<<"stdout to dev null"<<std::endl;
+    std::cerr<<"stderr is ok"<<std::endl;
+    fclose(stdout);
+    stdout = fdopen(0, "w");
+    std::cout<<"stdout is ok again"<<std::endl;
+}
+
+int main() {
+    redirect1();
+    redirect2();
+    return 0;
+}
