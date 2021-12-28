588c4292d79337e000f37b1eb645f2b7752b728a
DHacky <dhgacky@gmail.com>
2021-12-28 15:42:39 +0800

    Update lambda.cpp

--- cpplib/lambda.cpp
+++ cpplib/lambda.cpp
@@ -24,77 +24,70 @@ struct lam {
 // answer using functor, but not closure
 lam f3(int n) { return lam(n); }
 
+// answer using mutable lambda
+std::function<int(int)> f4(int n) {
+  return [=](int i) mutable { return n += i; };
+}
+
 int main(int argc, char const* argv[]) {
   int c = 10;
+  std::cout << "f1: ";
   auto r1 = f1(10);
+  for (int i = 0; i < c; ++i) {
+    // auto a = r1(i);
+    // std::cout << a << ",";
+    std::cout << r1(i) << ",";
+  }
+  std::cout << std::endl;
+
+  std::cout << "f2: ";
   auto r2 = f2(20);
   for (int i = 0; i < c; ++i) {
-    auto a = r1(i);
-    std::cout << "f1 " << a << std::endl;
+    // auto b = r2(i);
+    // std::cout << b << ",";
+    std::cout << r2(i) << ",";
   }
-  for (int i = 0; i < c; ++i) {
-    auto b = r2(i);
-    std::cout << "f2 " << b << std::endl;
-  }
+  std::cout << std::endl;
+
+  std::cout << "f3: ";
   auto r3 = f3(30);
   for (int i = 0; i < c; ++i) {
-    std::cout << "f3 " << r3(i) << std::endl;
+    std::cout << r3(i) << ",";
   }
-  
-  double pi = 3.1415926;
-  double r = 2.0;
+  std::cout << std::endl;
 
+  std::cout << "f4: ";
+  auto r4 = f4(40);
+  for (int i = 0; i < c; ++i) {
+    std::cout << r4(i) << ",";
+  }
+  std::cout << std::endl;
+
+  // answer using lambda
+  std::cout << "f5: ";
+  int init = 50;
   // Function object is not closure.
   // follow code will compile error:
-  // use of local variable with automatic storage from containing function pi
+  // use of local variable with automatic storage from containing function init
   /*
-  class CircleArea {
-   public:
-    CircleArea() {}
-    double operator()(double r) const { return pi * r * r; }
+  struct lam_error {
+    int operator()(int i) { return init += i; }
   };
-  CircleArea circleArea;
   */
-
-  // lambda is a closure
-  // lambda as closure like class as instance
-  auto circleArea = [=](double r) { return pi * r * r; };
-  double area = circleArea(r);
-  std::cout << area << std::endl;
+  // lambda is a closure, lambda as closure like class as instance
+  auto f5 = [&](int i) { return init += i; };
+  for (int i = 0; i < c; ++i) {
+    std::cout << f5(i) << ",";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
 /*
-output:
-f1 10
-f1 32767
-f1 32768
-f1 32769
-f1 32770
-f1 32771
-f1 32772
-f1 32773
-f1 32774
-f1 32775
-f2 0
-f2 1
-f2 3
-f2 6
-f2 10
-f2 15
-f2 21
-f2 28
-f2 36
-f2 45
-f3 30
-f3 31
-f3 33
-f3 36
-f3 40
-f3 45
-f3 51
-f3 58
-f3 66
-f3 75
-12.5664
+ouput:
+f1: 10,1,2,3,4,5,6,7,8,9,
+f2: 0,1,3,6,10,15,21,28,36,45,
+f3: 30,31,33,36,40,45,51,58,66,75,
+f4: 40,41,43,46,50,55,61,68,76,85,
+f5: 50,51,53,56,60,65,71,78,86,95,
 */
